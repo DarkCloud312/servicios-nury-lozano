0c6e03a904c53af969fa90c99595de47b2bd9fd6
LuisLozano <luisml312@gmail.com>
2020-05-25 13:11:19 -0500

    navbar viejita buggeada en movile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { CookieService } from 'ngx-cookie-service';
+import { VirtualTimeScheduler } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  private cookieMsgDissmise: string;
+  private cookieDissmise: boolean = false;
+
+  constructor(
+    private modalService: NgbModal,
+    private cookieService: CookieService
+  ) {}
+
+  public ngOnInit(): void {
+    this.cookieDissmise = this.cookieService.check('cookieDissmise');
+  }
+
+  open(content) {
+      this.modalService.open(content, {ariaDescribedBy: 'modal-basic-title'}).result.catch(
+        (error) => console.error(error)
+      );
+  }
+
   title = 'pagina-oferta-de-servicios';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { CookieService } from 'ngx-cookie-service';
+
+import { HomeComponent } from './home/home.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { InformacionComponent } from './informacion/informacion.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ServiciosComponent,
+    InformacionComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [ CookieService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
